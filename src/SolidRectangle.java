301b8c03d68a9b4bb12d8ee0a2c8c18f36cd831f
Sanjeev Kumar <sanjeev110592@gmail.com>
2022-08-08 19:24:09 +0530

    HollowRectangle

--- src/SolidRectangle.java
+++ src/SolidRectangle.java
@@ -1,7 +1,7 @@
 public class SolidRectangle {
     public static void main(String[] args){
-        int m = 6 ;
-        int n = 10 ;
+        int m = 6 ; //Row
+        int n = 10 ; //Column
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 System.out.print("* ") ;
