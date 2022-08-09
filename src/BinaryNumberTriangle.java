32371bcc3b7b888858971fcc20fff851519e2938
Sanjeev Kumar <sanjeev110592@gmail.com>
2022-08-09 23:47:48 +0530

    Triangle of binary numbers

---
+++ src/BinaryNumberTriangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BinaryNumberTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt() ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                if(i%2 == 0){
+                    if(j%2 == 0){
+                        System.out.print("1 ") ;
+                    }else{
+                        System.out.print("0 ") ;
+                    }
+                }else{
+                    if(j%2 == 0){
+                        System.out.print("0 ") ;
+                    }else{
+                        System.out.print("1 ") ;
+                    }
+                }
+            }
+            System.out.println(" ") ;
+        }
+    }
+}
